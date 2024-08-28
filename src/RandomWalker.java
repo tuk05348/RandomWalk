8036da17f365fe6209c125e7b99ebddee621c15f
Niaz Baharudeen <tuk05348@temple.edu>
2024-08-27 21:13:15 -0400

    Added a Point parameter to the RandomWalker to track its position.

--- src/RandomWalker.java
+++ src/RandomWalker.java
@@ -2,9 +2,11 @@
 public class RandomWalker {
 	
 	private int steps;
+	private Point point;
 	
 	public RandomWalker(int steps) {
 		this.steps = steps;
+		this.point = new Point(0, 0);
 	}
 
 	public static void main(String[] args) {
@@ -12,7 +14,7 @@ public class RandomWalker {
 			throw new IllegalArgumentException("Must have exactly one parameter.");
 		}
 		int steps = Integer.parseInt(args[0]);
-
+		RandomWalker walker = new RandomWalker(steps);
 	}
 	
 	public void walk() {
