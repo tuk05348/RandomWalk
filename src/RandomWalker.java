75b7df686c460d5ca4b5e391a9b2575fc8fee968
Niaz Baharudeen <tuk05348@temple.edu>
2024-08-27 20:34:07 -0400

    Threw an exception to catch an invalid number of arguments, there must be only one, the number of steps. Parsed number of steps from cmdline into an integer.

--- src/RandomWalker.java
+++ src/RandomWalker.java
@@ -8,8 +8,17 @@ public class RandomWalker {
 	}
 
 	public static void main(String[] args) {
-		
+		if(args.length != 1) { //throw exception when exactly 1 arg is not given
+			throw new IllegalArgumentException("Must have exactly one parameter.");
+		}
+		int steps = Integer.parseInt(args[0]);
 
 	}
+	
+	public void walk() {
+		
+	}
+	
+	
 
 }
